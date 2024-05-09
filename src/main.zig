a001764b6899eb72f37bddf518fdf12aed137ac8
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2024-05-09 17:58:48 +0200

    set up watches with inotify

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const logger = std.log.default;
 
 const Args = enum { help };
 
@@ -6,6 +7,9 @@ const Options = struct {
     fileList: std.ArrayList([]const u8),
     help: bool,
 };
+
+const stdout = std.io.getStdOut().writer();
+var stderr = std.io.getStdErr().writer();
 
 fn parseArgs(allocator: std.mem.Allocator) !Options {
     var options = Options{ .help = false, .fileList = std.ArrayList([]const u8).init(allocator) };
@@ -17,6 +21,7 @@ fn parseArgs(allocator: std.mem.Allocator) !Options {
         .{ "-h", Args.help },
     });
 
+    _ = args.next(); // skip the first arg which is the program name
     // All args with -- will be options, others will be files to watch
     while (args.next()) |arg| {
         if (argMap.get(arg)) |argEnum| {
@@ -47,19 +52,53 @@ pub fn main() !void {
             \\  -h, --help  Display this help message
             \\
         ;
-        std.debug.print(helpText, .{});
+        try stdout.print(helpText, .{});
+        return;
     }
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    // const stdout_file = std.io.getStdOut().writer();
-    // var bw = std.io.bufferedWriter(stdout_file);
-    // const stdout = bw.writer();
+    // init inotify
+    const inotify = std.posix.inotify_init1(0); // no flags, potentially can set IN_NONBLOCK and/or IN_CLOEXEC
 
-    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const inotify_fd = inotify catch {
+        try stderr.print("Failed to initialize inotify\n", .{});
+        return;
+    };
+    defer std.posix.close(inotify_fd);
 
-    // try bw.flush(); // don't forget to flush!
+    logger.debug("Initialized inotify with fd {d}", .{inotify_fd});
+
+    // add watch for all files in file list
+
+    const cwd = std.fs.cwd(); // current working directory
+
+    var wds = std.ArrayList(i32).init(allocator);
+    for (options.fileList.items) |file| {
+        logger.debug("Processing file {s}\n", .{file});
+        const stat = cwd.statFile(file) catch {
+            try stderr.print("Failed to stat file {s}\n", .{file});
+            continue;
+        };
+
+        if (stat.kind != .file) {
+            try stderr.print("File {s} is not a regular file. Ignored.\n", .{file});
+            continue;
+        }
+
+        const wd = std.posix.inotify_add_watch(inotify_fd, file, std.os.linux.IN.MODIFY) catch {
+            try stderr.print("Failed to add watch for file {s}\n", .{file});
+            continue;
+        };
+        try wds.append(wd);
+
+        logger.debug("Added watch for file {s} with wd {d}", .{ file, wd });
+    }
+    defer {
+        for (wds.items) |wd| {
+            std.posix.inotify_rm_watch(inotify_fd, wd);
+            logger.debug("Deferred — Removed watch for wd {d}", .{wd});
+        }
+        wds.deinit();
+    }
 }
 
 test "simple test" {
